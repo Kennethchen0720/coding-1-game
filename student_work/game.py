f85894482f135686f9fe79fa18601603ac189380
Kennethchen0720 <phoniexfire0720@gmail.com>
2026-02-20 15:05:16 +0000

    day 1

--- student_work/game.py
+++ student_work/game.py
@@ -1,7 +1,17 @@
 # Write your game here
+import curses
 
 game_data = {
-    # Store board dimensions, player/enemy positions, score, energy, collectibles, and icons
+    'width': 30,
+    'height': 30,
+    'player': {"x":0, "y":0, "score":0, "lives":1, "max_lives":1}
+    'basket_pos':{"x":1,"y":1}
+
+
+    #Emoji
+    'coins': "🪙"
+    'bomb': "💣"
+    ''
 }
 
 def draw_board(screen):
